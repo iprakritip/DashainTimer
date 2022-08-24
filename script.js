c2576c23d615df0aca7fd225f3f4b87200d8cdd6
Prakriti Pokharel <101075967+iprakritip@users.noreply.github.com>
2022-08-24 21:07:56 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,71 @@
-const daysEl = document.getElementById('days');
-const hoursEl = document.getElementById('hours');
-const minutesEl = document.getElementById('minutes');
-const secondsEl = document.getElementById('seconds');
+var festDate;
+  
+document.getElementById("festSelector").addEventListener("change",(event)=>{
+   
+    if (event.target.value= "newyear") {
+        festDate= ("13 Apr 2023");
+    } if(event.target.value= "teej"){
+        festDate= ("30 Aug 2022");
+    } else if(event.target.value= "dashain"){
+        festDate= ("5 Oct 2022");
+    } else if(event.target.value= "tihar"){
+        festDate= ("27 Oct 2022");
+    } else if(event.target.value= "chhath"){
+        festDate= ("30 Oct 2022");
+    } else if(event.target.value= "maghi"){
+        festDate= ("15 Jan 2023");
+    } else if(event.target.value= "shivaratri"){
+        festDate= ("18 Feb 2023");
+    }else if(event.target.value= "chaite"){
+        festDate= ("31 Mar 2023");
+    }else if(event.target.value= "holi1"){
+        festDate= ("6 Mar 2023");
+    }else if(event.target.value= "holi2"){
+        festDate= ("7 Mar 2023");
+    }
+    else{
+        console.log("Please select a festival");
+    }
+    console.log(festDate);
 
-const dashainTika = ("5 Oct 2022");
+});
+
+  
 
 
-function countdown(){
-    const dashainDate =new Date(dashainTika).getTime();
-    const currentDate= new Date();
-    const timeLeft= Math.abs(dashainDate-currentDate);
 
-    const totalSeconds = timeLeft/1000;
+// const daysEl = document.getElementById('days');
+// const hoursEl = document.getElementById('hours');
+// const minutesEl = document.getElementById('minutes');
+// const secondsEl = document.getElementById('seconds');
 
-    const seconds = Math.floor(totalSeconds) % 60;
-    const minutes = Math.floor(totalSeconds/60) % 60;
-    const hours = Math.floor(totalSeconds/3600) % 24;
-    const days = Math.floor(totalSeconds/3600/24);
-
-    daysEl.innerHTML = formatTime(days);
-    hoursEl.innerHTML=formatTime(hours);
-    minutesEl.innerHTML=formatTime(minutes);
-    secondsEl.innerHTML=formatTime(seconds);
-
-}
-
-function formatTime(time){
-    return (time < 10) ? "0"+time :time;
-}
+// const dashainTika = ("5 Oct 2022");
 
 
-countdown();
+// function countdown(){
+//     const dashainDate =new Date(dashainTika).getTime();
+//     const currentDate= new Date();
+//     const timeLeft= Math.abs(dashainDate-currentDate);
 
-setInterval(countdown,1000);
+//     const totalSeconds = timeLeft/1000;
+
+//     const seconds = Math.floor(totalSeconds) % 60;
+//     const minutes = Math.floor(totalSeconds/60) % 60;
+//     const hours = Math.floor(totalSeconds/3600) % 24;
+//     const days = Math.floor(totalSeconds/3600/24);
+
+//     daysEl.innerHTML = formatTime(days);
+//     hoursEl.innerHTML=formatTime(hours);
+//     minutesEl.innerHTML=formatTime(minutes);
+//     secondsEl.innerHTML=formatTime(seconds);
+
+// }
+
+// function formatTime(time){
+//     return (time < 10) ? "0"+time :time;
+// }
+
+
+// countdown();
+
+// setInterval(countdown,1000);
