72b40f7ba7254f66b139e2dc594f41b5c29421e5
Prakriti Pokharel <101075967+iprakritip@users.noreply.github.com>
2022-08-24 22:09:16 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
-var festDate;
+var festDate= new Date();
   
 document.getElementById("festSelector").addEventListener("change",(event)=>{
-   
-    if (event.target.value= "newyear") {
+//console.log(event.target.value);
+//festDate= "13 Apr 2023";   
+    if (event.target.value== "newyear") {
         festDate= ("13 Apr 2023");
-    } if(event.target.value= "teej"){
+        document.getElementById("festNaam").innerHTML = "Happy New Year";
+    } else if(event.target.value== "teej"){
         festDate= ("30 Aug 2022");
-    } else if(event.target.value= "dashain"){
+        document.getElementById("festNaam").innerHTML = "Haritalika Teej";
+    } else if(event.target.value== "dashain"){
         festDate= ("5 Oct 2022");
-    } else if(event.target.value= "tihar"){
+        document.getElementById("festNaam").innerHTML = "Vijaya Dashami";
+    } else if(event.target.value== "tihar"){
         festDate= ("27 Oct 2022");
-    } else if(event.target.value= "chhath"){
+        document.getElementById("festNaam").innerHTML = "Bhai Tika";
+    } else if(event.target.value== "chhath"){
         festDate= ("30 Oct 2022");
-    } else if(event.target.value= "maghi"){
+        document.getElementById("festNaam").innerHTML = "Chhath Parva";
+    } else if(event.target.value== "maghi"){
         festDate= ("15 Jan 2023");
-    } else if(event.target.value= "shivaratri"){
+        document.getElementById("festNaam").innerHTML = "Maghe Sakranti";
+    } else if(event.target.value== "shivaratri"){
         festDate= ("18 Feb 2023");
-    }else if(event.target.value= "chaite"){
+        document.getElementById("festNaam").innerHTML = "Maha Shivaratri";
+    }else if(event.target.value== "chaite"){
         festDate= ("31 Mar 2023");
-    }else if(event.target.value= "holi1"){
+        document.getElementById("festNaam").innerHTML = "Chaite Dasahin";
+    }else if(event.target.value== "holi1"){
         festDate= ("6 Mar 2023");
-    }else if(event.target.value= "holi2"){
+        document.getElementById("festNaam").innerHTML = "Pahad Holi";
+    }else if(event.target.value== "holi2"){
         festDate= ("7 Mar 2023");
+        document.getElementById("festNaam").innerHTML = "Terai Holi";
     }
     else{
         console.log("Please select a festival");
     }
     console.log(festDate);
+
+    document.getElementById('jyan').style.backgroundImage= "url(images/"+event.target.value+".jpg)";
+    // backgroundImage = event.target.value + ".png";
 
 });
 
@@ -34,38 +48,38 @@ document.getElementById("festSelector").addEventListener("change",(event)=>{
 
 
 
-// const daysEl = document.getElementById('days');
-// const hoursEl = document.getElementById('hours');
-// const minutesEl = document.getElementById('minutes');
-// const secondsEl = document.getElementById('seconds');
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
 
-// const dashainTika = ("5 Oct 2022");
-
-
-// function countdown(){
-//     const dashainDate =new Date(dashainTika).getTime();
-//     const currentDate= new Date();
-//     const timeLeft= Math.abs(dashainDate-currentDate);
-
-//     const totalSeconds = timeLeft/1000;
-
-//     const seconds = Math.floor(totalSeconds) % 60;
-//     const minutes = Math.floor(totalSeconds/60) % 60;
-//     const hours = Math.floor(totalSeconds/3600) % 24;
-//     const days = Math.floor(totalSeconds/3600/24);
-
-//     daysEl.innerHTML = formatTime(days);
-//     hoursEl.innerHTML=formatTime(hours);
-//     minutesEl.innerHTML=formatTime(minutes);
-//     secondsEl.innerHTML=formatTime(seconds);
-
-// }
-
-// function formatTime(time){
-//     return (time < 10) ? "0"+time :time;
-// }
+//festDate = ("5 Oct 2022");
 
 
-// countdown();
+function countdown(){
+    const dashainDate =new Date(festDate).getTime();
+    const currentDate= new Date();
+    const timeLeft= Math.abs(dashainDate-currentDate);
 
-// setInterval(countdown,1000);
+    const totalSeconds = timeLeft/1000;
+
+    const seconds = Math.floor(totalSeconds) % 60;
+    const minutes = Math.floor(totalSeconds/60) % 60;
+    const hours = Math.floor(totalSeconds/3600) % 24;
+    const days = Math.floor(totalSeconds/3600/24);
+
+    daysEl.innerHTML = formatTime(days);
+    hoursEl.innerHTML=formatTime(hours);
+    minutesEl.innerHTML=formatTime(minutes);
+    secondsEl.innerHTML=formatTime(seconds);
+
+}
+
+function formatTime(time){
+    return (time < 10) ? "0"+time :time;
+}
+
+
+countdown();
+
+setInterval(countdown,1000);
