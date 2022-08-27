4e07f17efbeac80341f3b47897cffac36d9c198d
iprakritip <prakritikokam@gmail.com>
2022-08-27 21:35:43 +0545

    all files ready with changing headline description

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.getElementById("festSelector").addEventListener("change",(event)=>{
     if (event.target.value== "newyear") {
         festDate= ("13 Apr 2023");
         document.getElementById("festNaam").innerHTML = "Happy New Year";
-        changeColor("#000");
+        changeColor("#fff");
     } else if(event.target.value== "teej"){
         festDate= ("30 Aug 2022");
         document.getElementById("festNaam").innerHTML = "Haritalika Teej";
@@ -17,6 +17,7 @@ document.getElementById("festSelector").addEventListener("change",(event)=>{
     } else if(event.target.value== "dashain"){
         festDate= ("5 Oct 2022");
         document.getElementById("festNaam").innerHTML = "Vijaya Dashami";
+        changeColor("#000");
     } else if(event.target.value== "tihar"){
         festDate= ("27 Oct 2022");
         document.getElementById("festNaam").innerHTML = "Bhai Tika";
@@ -24,23 +25,27 @@ document.getElementById("festSelector").addEventListener("change",(event)=>{
     } else if(event.target.value== "chhath"){
         festDate= ("30 Oct 2022");
         document.getElementById("festNaam").innerHTML = "Chhath Parva";
+        changeColor("#000");
     } else if(event.target.value== "maghi"){
         festDate= ("15 Jan 2023");
         document.getElementById("festNaam").innerHTML = "Maghe Sakranti";
+        changeColor("#000");
     } else if(event.target.value== "shivaratri"){
         festDate= ("18 Feb 2023");
         document.getElementById("festNaam").innerHTML = "Maha Shivaratri";
+        changeColor("#fff");
     }else if(event.target.value== "chaite"){
         festDate= ("31 Mar 2023");
         document.getElementById("festNaam").innerHTML = "Chaite Dasahin";
+        changeColor("#000");
     }else if(event.target.value== "holi1"){
         festDate= ("6 Mar 2023");
         document.getElementById("festNaam").innerHTML = "Pahad Holi";
-        // document.getElementById("heading1").style.color="#fff";
-
+        changeColor("#fff");
     }else if(event.target.value== "holi2"){
         festDate= ("7 Mar 2023");
         document.getElementById("festNaam").innerHTML = "Terai Holi";
+        changeColor("#fff");
     }
     else{
         console.log("Please select a festival");
